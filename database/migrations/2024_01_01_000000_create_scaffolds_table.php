6de6059991cfddfac7b776c47cea0353e26485ad
Talemul Islam <talemul@mislbd.com>
2025-08-14 10:36:43 +0600

    update schema details

--- database/migrations/2024_01_01_000000_create_scaffolds_table.php
+++ database/migrations/2024_01_01_000000_create_scaffolds_table.php
@@ -30,13 +30,17 @@ class CreateScaffoldsTable extends Migration
             $table->foreignId('scaffold_id')
                 ->constrained($scaffoldTable)
                 ->onDelete('cascade');
-            $table->string('name')->nullable();
-            $table->string('type')->nullable();
-            $table->boolean('nullable')->default(false);
-            $table->string('key')->nullable();
-            $table->string('default')->nullable();
-            $table->string('comment')->nullable();
-            $table->integer('order')->default(0);
+            $table->string('name')->nullable()->comment('table column name');
+            $table->string('type')->nullable()->comment('table column type');
+            $table->boolean('nullable')->default(false)->comment('table column nullable or not');
+            $table->string('key')->nullable()->comment('table column key null index, unique');
+            $table->string('default')->nullable()->comment('table column default value');
+            $table->string('comment')->nullable()->comment('table column comment or description');
+            $table->integer('order')->default(0)->comment('table column order');
+            $table->string('input_type', 40)->nullable()->comment('text, email, date, radio, select, checkbox, file, textarea...');        // text, email, date, radio, select, checkbox, file, textarea...
+            $table->string('options_source', 191)->nullable()->comment('static OR FQCN like App\Models\User');   // 'static' OR FQCN like 'App\Models\User'
+            $table->string('options_value_col', 191)->nullable()->comment('static: "male,female,other" | model: "id"');// static: "male,female,other" | model: "id"
+            $table->string('options_label_col', 191)->nullable()->comment('static: "Male,Female,Other"  | model: "name"');// static: "Male,Female,Other"  | model: "name"
             $table->timestamps();
         });
     }
@@ -50,8 +54,8 @@ class CreateScaffoldsTable extends Migration
     public function down()
     {
         $connection = config('admin.database.connection') ?: config('database.default');
-        Schema::connection($connection)->dropIfExists( 'helper_scaffolds');
-        Schema::connection($connection)->dropIfExists( 'helper_scaffold_details');
+        Schema::connection($connection)->dropIfExists('helper_scaffolds');
+        Schema::connection($connection)->dropIfExists('helper_scaffold_details');
     }
 }
 
